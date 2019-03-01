f8d94ab0197b3249d6625d5e0df251cb81793c84
Source of 0 <SourceOf0@gmail.com>
2019-03-01 11:10:23 +0900

    init

---
+++ addMaskTag.rb
@@ -0,0 +1,86 @@
+require "pathname"
+
+source_dir = Pathname("source")
+destination_dir = Pathname("destination")
+
+Pathname.glob(source_dir.join("**/*")) do |source_path|
+  # ファイルじゃないとき（ディレクトリーのとき）はスキップ
+  next unless source_path.file?
+
+  # source_dir から見た相対パス
+  rel_path = source_path.relative_path_from source_dir
+
+  # 保存先のパス
+  destination_path = destination_dir.join(rel_path)
+
+  # ディレクトリーを必要に応じて作成
+  destination_path.dirname.mkpath
+
+  elementCount = 0
+  elementStr = ""
+  
+  # 書き出し
+  destination_path.open "w" do |f|
+  
+    # 元ファイルの各行について繰り返し
+    source_path.each_line do |line|
+    
+      if line.include?("Mtarget") then
+        # ターゲット用の属性を付与
+        f.puts line.sub(">", ' clip-path="url(#mask_target)">')
+
+      elsif line.include?("Mask") then
+        f.puts line
+        
+        # マスク用データを初期化
+        elementStr = '<clipPath id="mask_target">' + "\n"
+        elementCount = 1
+        #f.puts "[FOUND]"
+        
+      elsif elementCount > 0 then
+        
+        if line.start_with?("<path") then
+          # マスク用データに格納しつつ出力
+          elementStr += line
+          f.puts line
+          
+        elsif line.start_with?("</") then
+          if elementCount == 1 then
+            # マスク該当箇所終端のためマスクを出力
+            elementStr += "</clipPath>"
+            f.puts elementStr
+            elementCount = 0
+            
+            # 元データの終端を出力
+            f.puts line
+            #f.puts "[END]"
+            
+          else
+            # マスク用データに格納しつつ出力
+            elementStr += line
+            f.puts line
+            
+            # 要素の階層を下げる
+            elementCount -= 1
+            #f.puts "[DOWN]"
+          end
+          
+        elsif line.start_with?("<") then
+          # マスク用データに格納しつつ出力
+          elementStr += line
+          f.puts line
+          
+          # 要素の階層を上げる
+          elementCount += 1
+          #f.puts "[ADD]"
+        end
+
+      else
+        # そのまま出力
+        f.puts line
+      end
+      
+    end
+  end
+  
+end
